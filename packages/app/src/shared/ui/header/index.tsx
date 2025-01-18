923a0c511b645a8ae7fa34883e155596742b1f90
yend724 <65233817+yend724@users.noreply.github.com>
2025-01-18 18:29:17 +0900

    index.tsx を更新wip

--- packages/app/src/shared/ui/header/index.tsx
+++ packages/app/src/shared/ui/header/index.tsx
@@ -1,7 +1,7 @@
 export const Header: React.FC = () => {
   return (
     <header className="mx-auto w-full max-w-7xl">
-      <h1 className="font-bold text-xl">CSV to Markdown Table</h1>
+      <h1 className="font-bold text-xl">CSV 2 Markdown Table</h1>
     </header>
   );
 };
