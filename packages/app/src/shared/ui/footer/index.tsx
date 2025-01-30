ee567f83b9483fd6c76ae470e05fe99b30c07c9d
yend724 <65233817+yend724@users.noreply.github.com>
2025-01-30 11:03:04 +0900

    wip

--- packages/app/src/shared/ui/footer/index.tsx
+++ packages/app/src/shared/ui/footer/index.tsx
@@ -1,7 +1,7 @@
 export const Footer: React.FC = () => {
   return (
     <footer className="text-center">
-      <small className="text-xs">All rights reserved 2024, YEND.</small>
+      <small className="text-xs">All rights reserved 2025, YEND.</small>
     </footer>
   );
 };
